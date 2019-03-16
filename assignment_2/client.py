44c226927f3296dbdc891e50ffb49cef9e5f79e7
SeongJin <tjdwls0607@naver.com>
2019-03-16 19:17:34 +0900

    assignment_2 final

--- assignment_2/client.py
+++ assignment_2/client.py
@@ -1,18 +1,32 @@
+## client.py
+
 import socket
 import argparse
 
-def run(host, port):
+def run(host, port, strList):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
-        line = input(':')
-        s.sendall(line.encode())
+        msg = reverseMsg(strList)
+        s.sendall(msg.encode())
+
         resp = s.recv(1024)
         print(resp.decode())
 
+## 입력한 문자열을 반대로 뒤집어 주는 함수
+def reverseMsg(strList):
+    str = " ".join(strList)     ## 리스트 요소 사이에 공백을 넣어 스트링으로 변환
+    size = len(str)
+    reverseStr=''
+    for i in range(size-1, -1, -1):
+        reverseStr+=str[i]
+    
+    return reverseStr
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Echo client -p port -i host")
+    parser = argparse.ArgumentParser(description="Echo client -p port -i host -s string")
     parser.add_argument('-p', help="port_number", required=True)
-    parser.add_argument('-i', help="host_name", required=True) 
+    parser.add_argument('-i', help="host_name", required=True)
+    parser.add_argument('-s', help="input_string", nargs='+', required=True)
 
     args = parser.parse_args()
-    run(host=args.i, port=int(args.p))       
+    run(host=args.i, port=int(args.p), strList=args.s)
--- assignment_2/server.py
+++ assignment_2/server.py
@@ -1,16 +1,18 @@
+## sever.py
+
 import socket
 import argparse
 
 def run_server(port=4000):
-    host = ''
+    host = ''   ## 127.0.0.1 Loop back
 
     with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as s:
         s.bind((host, port))
-        s.listen(1)
+        s.listen(1)     ## max 1 client
 
         conn, addr = s.accept()
         msg = conn.recv(1024)
-        print(msg.decode())
+        print(msg.decode())     ## msg is a binary data, so we need to decode it
 
         conn.sendall(msg)
         conn.close()
